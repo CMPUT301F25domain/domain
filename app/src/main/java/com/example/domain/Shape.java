f1e571a16d41a09377ba6754551d8df928707b70
Kei-Iwashita <kei.iwashita@gmail.com>
2025-09-24 18:50:57 -0600

    Update Shape.java

--- app/src/main/java/com/example/domain/Shape.java
+++ app/src/main/java/com/example/domain/Shape.java
@@ -3,10 +3,7 @@ package com.example.domain;
 public abstract class Shape {
     protected int x;
     protected int y;
-<<<<<<< Updated upstream
-}
-=======
     protected String color = "red";
     protected String color = "black";
 }
->>>>>>> Stashed changes
+
