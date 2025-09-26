47d619b4f44b701fc882d0b6b63aeab739231800
Tanjil Rafi <tanjilsarker77@gmail.com>
2025-09-26 15:24:56 -0600

    Resolve merge conflicts in Shape.java

--- app/src/main/java/com/example/domain/Shape.java
+++ app/src/main/java/com/example/domain/Shape.java
@@ -4,6 +4,11 @@ public abstract class Shape {
     protected int x;
     protected int y;
 
+<<<<<<< Updated upstream
     protected String color = "blue";
 
 }
+=======
+    protected String color = "red";
+}
+>>>>>>> Stashed changes
